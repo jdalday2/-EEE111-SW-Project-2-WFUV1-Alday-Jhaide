60d47979a4946725b256324e8bd65fbd95ee23f9
jdalday2 <156769561+jdalday2@users.noreply.github.com>
2024-01-17 10:43:39 +0800

    Add files via upload

---
+++ [EEE111][SW Project 2][WFUV1] Alday, Jhaide/src/LibDbEntry.py
@@ -0,0 +1,12 @@
+class LibDbEntry:
+    def __init__(self,
+                 isbn=1,
+                 title='Book Title',
+                 genre='Fiction',
+                 publication='2000',
+                 availability='Available'):
+        self.isbn = isbn
+        self.title = title
+        self.genre = genre
+        self.publication = publication
+        self.availability = availability
